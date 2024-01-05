5980adde904f179b6fd68b3c2c744c0fa5a07b3a
Enrica Garcia <enrica.garcia@vasscompany.com>
2024-01-05 14:51:13 +0100

    feat: add select general component

--- product-feedback-app/src/app/add-feedback/add-feedback.component.ts
+++ product-feedback-app/src/app/add-feedback/add-feedback.component.ts
@@ -10,5 +10,5 @@ import { RouterLink } from '@angular/router';
   styleUrl: './add-feedback.component.scss'
 })
 export class AddFeedbackComponent {
-  title: string = 'Title';
+  title: string = 'Create New Feedback';
 }
