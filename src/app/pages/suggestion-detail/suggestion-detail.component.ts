7834c05dfe1b63fa489f1e203601d1cb60880f99
Enrica Garcia <enrica.garcia@vasscompany.com>
2024-01-22 08:26:53 +0100

    feat: add comment card to suggestion detail

--- src/app/pages/suggestion-detail/suggestion-detail.component.ts
+++ src/app/pages/suggestion-detail/suggestion-detail.component.ts
@@ -1,19 +1,69 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { Suggestion } from './suggestion.interface';
+import { CardComponent } from 'app/shared/components/card/card.component';
+import { CommentCardComponent } from 'app/shared/comment-card/comment-card.component';
 
 @Component({
   selector: 'app-suggestion-detail',
   standalone: true,
-  imports: [],
+  imports: [CardComponent, CommentCardComponent, RouterLink],
   templateUrl: './suggestion-detail.component.html',
   styleUrl: './suggestion-detail.component.scss'
 })
   export class SuggestionDetailComponent implements OnInit {
     id: string;
 
-    constructor (
-      private activateRouter: ActivatedRoute
-    ) { }
+    suggestion: Suggestion =  {
+      "id": 2,
+      "title": "Add a dark theme option",
+      "category": "feature",
+      "upvotes": 99,
+      "status": "suggestion",
+      "description": "It would help people with light sensitivities and who prefer dark mode.",
+      "comments": [
+        {
+          "id": 3,
+          "content": "Also, please allow styles to be applied based on system preferences. I would love to be able to browse Frontend Mentor in the evening after my device’s dark mode turns on without the bright background it currently has.",
+          "user": {
+            "image": "assets/user-images/image-elijah.jpg",
+            "name": "Elijah Moss",
+            "username": "hexagon.bestagon"
+          }
+        },
+        {
+          "id": 4,
+          "content": "Second this! I do a lot of late night coding and reading. Adding a dark theme can be great for preventing eye strain and the headaches that result. It’s also quite a trend with modern apps and  apparently saves battery life.",
+          "user": {
+            "image": "./assets/user-images/image-james.jpg",
+            "name": "James Skinner",
+            "username": "hummingbird1"
+          },
+          "replies": [
+            {
+              "content": "While waiting for dark mode, there are browser extensions that will also do the job. Search for 'dark theme' followed by your browser. There might be a need to turn off the extension for sites with naturally black backgrounds though.",
+              "replyingTo": "hummingbird1",
+              "user": {
+                "image": "./assets/user-images/image-anne.jpg",
+                "name": "Anne Valentine",
+                "username": "annev1990"
+              }
+            },
+            {
+              "content": "Good point! Using any kind of style extension is great and can be highly customizable, like the ability to change contrast and brightness. I'd prefer not to use one of such extensions, however, for security and privacy reasons.",
+              "replyingTo": "annev1990",
+              "user": {
+                "image": "./assets/user-images/image-ryan.jpg",
+                "name": "Ryan Welles",
+                "username": "voyager.344"
+              }
+            }
+          ]
+        }
+      ]
+    };
+
+    private activateRouter = inject(ActivatedRoute);
 
     ngOnInit(): void {
       this.id = this.activateRouter.snapshot.params['id'];
